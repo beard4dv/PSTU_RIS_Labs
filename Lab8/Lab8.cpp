3603a0fa1ff1450288c61d9668c6596047769dde
beard4dv <galanov.yura@gmail.com>
2025-05-20 19:00:04 +0500

    Add files via upload

---
+++ Lab8/Lab8.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include "windows.h"
+#include "locale"
+
+// Инициализация структуры данных пациентов
+struct patient
+{
+    char surname[20];
+    char name[20];
+    char patronymic[20];
+    char homeAddress[50];
+    int medCardNumber;
+    int insPolNumber;
+};
+
+// Функци ввода данных пациента
+patient gets()
+{
+    patient p;
+    bool error = true;
+    std::cout << "Введите данные пациента.\n";
+    std::cout << "Фамилия: "; std::cin >> p.surname;
+    std::cout << "Имя: "; std::cin >> p.name;
+    std::cout << "Отчество: "; std::cin >> p.patronymic;
+    std::cout << "Адрес: "; std::cin >> p.homeAddress;
+    while (error)
+    {
+        std::cout << "Номер медкарты: "; std::cin >> p.medCardNumber;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "Ошибка: введен некорректный символ. Введите число.\n";
+        }
+        else
+        {
+            std::cin.ignore(1000, '\n');
+            error = false;
+        }
+    }
+    error = true;
+    while (error)
+    {
+        std::cout << "Номер страхового полиса: "; std::cin >> p.insPolNumber;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "Ошибка: введен некорректный символ. Введите число.\n";
+        }
+        else
+        {
+            std::cin.ignore(1000, '\n');
+            error = false;
+        }
+    }
+    return p;
+}
+
+//  Функция создания бинарного файла с данными пациентов
+void createData()
+{
+    FILE* data;
+    data = fopen("data.bin", "wb");
+    if (!data)
+    {
+        std::cout << "Ошибка создания файла!!!\n";
+        return;
+    }
+    int n;
+    std::cout << "Введите кол-во добавляемых пациентов :";
+    std::cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        patient p = gets();
+        fwrite(&p, sizeof(patient), 1, data);
+    }
+    fclose(data);
+    system("cls");
+    std::cout << "Файл создан.\n";
+}
+
+//  Функция печати файла с данными пациентов
+void printData()
+{
+    FILE* data;
+    data = fopen("data.bin", "rb");
+    if (!data)
+    {
+        std::cout << "Ошибка открытия файла!!!\n";
+        return;
+    }
+    patient p;
+    system("cls");
+    std::cout << "Список пациентов:\n";
+    for (int i = 0; fread(&p, sizeof(patient), 1, data); i++)
+    {
+        std::cout << i+1 << ": " << "Фамилия: " << p.surname << ", Имя: " << p.name
+            << ", Отчество: " << p.patronymic << ", Адрес: " << p.homeAddress
+            << ", Номер медкарты: " << p.medCardNumber
+            << ", Номер страхового полиса: " << p.insPolNumber << std::endl;
+    }
+    fclose(data);
+}
+
+//  Функция удаления пациента по номеру медкарты
+void deletePatient()
+{
+    FILE* data;
+    data = fopen("data.bin", "rb");
+    FILE* tempData;
+    tempData = fopen("tempdata.bin", "wb");
+    if (!data || !tempData)
+    {
+        std::cout << "Ошибка открытия файлов!!!\n";
+        return;
+    }
+    int delMedCardNumber;
+    std::cout << "Введите номер карты удаляемого пациента: "; std::cin >> delMedCardNumber;
+    patient p;
+    bool found = false;
+    for (int i = 0; fread(&p, sizeof(patient), 1, data); i++)
+    {
+        if (p.medCardNumber == delMedCardNumber)
+        {
+            found = true;
+        }
+        else fwrite(&p, sizeof(patient), 1, tempData);
+    }
+    fclose(data);
+    fclose(tempData);
+    if (found)
+    {
+        data = fopen("data.bin", "wb");
+        tempData = fopen("tempdata.bin", "rb");
+        for (int i = 0; fread(&p, sizeof(patient), 1, tempData); i++)
+        {
+            fwrite(&p, sizeof(patient), 1, data);
+        }
+        fclose(data);
+        fclose(tempData);
+        remove("tempdata.bin");
+        system("cls");
+        std::cout << "Пациент с номером карты " << delMedCardNumber << " удален.\n";
+    }
+    else
+    {
+        system("cls");
+        std::cout << "Пациент с номером карты " << delMedCardNumber << " не найден.\n";
+        remove("tempData.bin");
+    }
+}
+
+// Функция добавления двух пациентов в начало файла
+void addPatient()
+{
+    FILE* data;
+    data = fopen("data.bin", "rb");
+    FILE* tempData;
+    tempData = fopen("tempdata.bin", "wb");
+    if (!data || !tempData)
+    {
+        std::cout << "Ошибка открытия файлов!!!\n";
+        return;
+    }
+    patient p1 = gets();
+    patient p2 = gets();
+    fwrite(&p1, sizeof(patient), 1, tempData);
+    fwrite(&p2, sizeof(patient), 1, tempData);
+    patient p;
+    for (int i = 0; fread(&p, sizeof(patient), 1, data); i++)
+    {
+        fwrite(&p, sizeof(patient), 1, tempData);
+    }
+    fclose(data);
+    fclose(tempData);
+    data = fopen("data.bin", "wb");
+    tempData = fopen("tempdata.bin", "rb");
+    for (int i = 0; fread(&p, sizeof(patient), 1, tempData); i++)
+    {
+        fwrite(&p, sizeof(patient), 1, data);
+    }
+    fclose(data);
+    fclose(tempData);
+    remove("tempdata.bin");
+    system("cls");
+    std::cout << "Два пациента добавлены в начало файла\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int selectionMenu;
+    std::cout << "###Карты пациентов.###\n";
+    do
+    {
+        std::cout << "Меню:\n";
+        std::cout << "1. Создать файл\n";
+        std::cout << "2. Вывод данных пациентов\n";
+        std::cout << "3. Удалить пациента по номеру медкарты\n";
+        std::cout << "4. Добавить двух пациентов в начало\n";    
+        std::cout << "0. Выход\n";
+        std::cout << "Выберите действие: ";
+        std::cin >> selectionMenu;
+        switch (selectionMenu)
+        {
+        case 1: createData(); break;
+        case 2: printData(); break;
+        case 3: deletePatient(); break;
+        case 4: addPatient(); break;
+        case 0: std::cout << "Завершение программы..." << std::endl; break;
+        default: system("cls"); std::cout << "Ошибка выбора пункта меню\n";
+        }
+    } while (selectionMenu != 0);
+}
---
+++ Lab9/Lab9.cpp
@@ -0,0 +1,168 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include "windows.h"
+#include "locale"
+
+bool checkWord(const char* buf)
+{
+    int count = 0;
+    bool word = false;
+    for (int i = 0; buf[i] != '\0'; ++i)
+    {
+        if (buf[i] == ' ')
+        {
+            word = false;
+        }
+        else if (!word)
+        {
+            word = true;
+            count++;
+            if (count > 1) return false;
+        }
+    }
+    return count == 1;
+}
+
+void createFile()
+{
+    FILE* F1;
+    F1 = fopen("F1.txt", "w");
+    if (!F1)
+    {
+        std::cout << "Ошибка создания файла!!!\n";
+        return;
+    }
+    char input[255];
+    std::cout << "Запись строк в файл F1.txt\n";
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Строка " << i + 1 << "/10: ";
+        std::cin.getline(input, sizeof(input));
+        fputs(input, F1);
+        fputs("\n", F1);
+    }
+    fclose(F1);
+}
+
+void stringCopy()
+{
+    FILE* F1;
+    F1 = fopen("F1.txt", "r");
+    FILE* F2;
+    F2 = fopen("F2.txt", "w");
+    if (!F1 || !F2) 
+    {
+        std::cout << "Ошибка открытия/создания файла!!!\n";
+        return;
+    }
+    const int MAXLINE = 255;
+    char buf[MAXLINE];
+    bool check = false;
+    while (fgets(buf, MAXLINE, F1))
+    {
+        if (checkWord(buf))
+        {
+            check = true;
+            fputs(buf, F2);
+        }
+    }
+    fclose(F1);
+    fclose(F2);
+    if (check)
+    {
+        std::cout << "Строки с одним словом добалены в файл F2.\n";
+    }
+    else std::cout << "Строки с одним словом не найдены.\n";
+}
+
+void longWord()
+{
+    FILE* F2;
+    F2 = fopen("F2.txt", "r");
+    if (!F2)
+    {
+        std::cout << "Ошибка открытия файла!!!\n";
+        return;
+    }
+    const int MAXLINE = 255;
+    char buf[MAXLINE];
+    char longWord[MAXLINE] = "\0";
+    while (fgets(buf, MAXLINE, F2))
+    {
+        if (strlen(buf) > strlen(longWord))
+        {
+            strcpy(longWord, buf);
+        }
+    }
+    std::cout << "Самое длинное слово в файле F2: " << longWord << std::endl;
+    fclose(F2);
+}
+
+void printFile()
+{
+    int subMenu;
+    char buf[255];
+    std::cout << "1. F1\n2. F2\nВыберите файл: "; std::cin >> subMenu;
+    switch (subMenu)
+    {
+    case 1:
+    {
+        FILE* F1;
+        F1 = fopen("F1.txt", "r");
+        if (!F1)
+        {
+            std::cout << "Ошибка открытия файла!!!\n";
+            return;
+        }
+        std::cout << "Cодержимое файла F1\n";
+        while (fgets(buf, 255, F1))
+        {
+            std::cout << buf;
+        }
+        fclose(F1);
+        break;
+    }
+    case 2:
+    {
+        FILE* F2;
+        F2 = fopen("F2.txt", "r");
+        if (!F2)
+        {
+            std::cout << "Ошибка открытия файла!!!\n";
+            return;
+        }
+        std::cout << "Cодержимое файла F2\n";
+        while (fgets(buf, 255, F2))
+        {
+            std::cout << buf;
+        }
+        fclose(F2);
+        break;
+    }
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int selectionMenu;
+    do
+    {
+        std::cout << "Ввод-вывод строк. Меню:\n";
+        std::cout << "1. Создать файл\n2. Копировать строки с одним словом\n3. Вывести самое длинное слово в файле F2\n4. Вывод файлов\nВыберите действие: ";
+        std::cin >> selectionMenu;
+        std::cin.ignore();
+        switch (selectionMenu)
+        {
+        case 1: createFile(); break;
+        case 2: stringCopy(); break;
+        case 3: longWord(); break;
+        case 4: printFile(); break;
+        case 0: std::cout << "Exit..."; break;
+        default: std::cout << "Ошибка выбора пункта меню\n";
+        }
+    } while (selectionMenu != 0);
+}
